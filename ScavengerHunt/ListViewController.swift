e81119d3bee425a6bec52607f6dbecb0fa35fd98
Henry Saniuk <henry@henrysaniuk.com>
2015-02-02 18:56:19 -0500

    Added UITableView

---
+++ ScavengerHunt/ListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListViewController.swift
+//  ScavengerHunt
+//
+//  Created by Henry Saniuk on 2/2/15.
+//  Copyright (c) 2015 Henry Saniuk. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    var itemsList = ["Cat", "Bird", "Brick"]
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsList.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListViewCell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel?.text = itemsList[indexPath.row]
+        return cell
+    }
+}
