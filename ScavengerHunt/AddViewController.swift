4a6e7c386364d48dbc869f5e080bd1b872d334a5
Henry Saniuk <henry@henrysaniuk.com>
2015-02-02 20:34:24 -0500

    Connected add and cancel buttons

--- ScavengerHunt/AddViewController.swift
+++ ScavengerHunt/AddViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AddViewController.swift
 //  ScavengerHunt
 //
 //  Created by Henry Saniuk on 2/2/15.
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AddViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func cancel(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
